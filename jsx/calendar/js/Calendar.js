9e7e3d934ff3052267329031a3cb288d8754a5ea
Nikita Skrebets <angelhearted@ya.ru>
2018-06-30 13:23:01 +0300

    Completed calendar

--- jsx/calendar/js/Calendar.js
+++ jsx/calendar/js/Calendar.js
@@ -1,5 +1,5 @@
 function Calendar({date}) {
-  const weekDayHeaderTop = (
+  const mainHeader = (
     <div className="ui-datepicker-material-header">
       <div className="ui-datepicker-material-day">{getWeekDay()}</div>
       <div className="ui-datepicker-material-date">
@@ -7,16 +7,14 @@ function Calendar({date}) {
         <div className="ui-datepicker-material-month">{getMonthName()}</div>
         <div className="ui-datepicker-material-year">{date.getFullYear()}</div>
       </div>
-    </div>
-  );
-  const currentDateHeader = (
+    </div>);
+  const monthYearHeader = (
     <div className="ui-datepicker-header">
       <div className="ui-datepicker-title">
         <span className="ui-datepicker-month">{getMonthName()}
         </span>&nbsp;<span className="ui-datepicker-year">{date.getFullYear()}</span>
       </div>
-    </div>
-  );
+    </div>);
 
   const weekColumns = (
     <colgroup>
@@ -27,8 +25,7 @@ function Calendar({date}) {
       <col />
       <col className="ui-datepicker-week-end" />
       <col className="ui-datepicker-week-end" />
-    </colgroup>
-  );
+    </colgroup>);
   const weekDaysHeader = (
     <thead>
       <tr>
@@ -40,41 +37,71 @@ function Calendar({date}) {
         <th scope="col" title="Суббота">Сб</th>
         <th scope="col" title="Воскресенье">Вс</th>
       </tr>
-    </thead>
-  );
+    </thead>);
+  
+  const today = date.getDate();
+  const displayedDates = (() => {
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const firstMonthDay = new Date(year, month, 1);
+
+    // if Sunday (0) - set 7
+    const weekDaysBefore1stDay = (firstMonthDay.getDay() || 6) - 1;
+    const firstDisplayedDay = new Date(year, month, 1 - weekDaysBefore1stDay);
+    const lastMonthDate = new Date(year, month + 1, 0).getDate();
+
+    const result = [[]];
+    let i = firstDisplayedDay.getDate();
+    // for other months days are added with minus
+    let monthMult = weekDaysBefore1stDay === 0 ? 1 : -1;
+    for (; result[0].length < 7; i++) {
+      result[0].push(monthMult * i);
+      if (weekDaysBefore1stDay === result[0].length) {
+        monthMult = 1;
+        i = 0;
+      }
+    }
+
+    let lastRow;
+    for (; i <= lastMonthDate; i++) {
+      lastRow = result[result.length - 1];
+      lastRow.length < 7 ? lastRow.push(i) : result.push([i]);
+    }
+
+    lastRow = result[result.length - 1];
+    for (i = 1; lastRow.length < 7; i++)  {
+      lastRow.push(-i);
+    }
+
+    return result;
+  })();
+
 
   return (
     <div className="ui-datepicker">
-      {weekDayHeaderTop}
-      {currentDateHeader}
+      {mainHeader}
+      {monthYearHeader}
       <table className="ui-datepicker-calendar">
         {weekColumns}
         {weekDaysHeader}
         <tbody>
-          
-          <tr>
-            <td className="ui-datepicker-other-month">27</td>
-            <td className="ui-datepicker-other-month">28</td>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-          </tr>
-          <tr>
-            <td>6</td>
-            <td>7</td>
-            <td className="ui-datepicker-today">8</td>
-            <td>9</td>
-            <td>10</td>
-            <td>11</td>
-            <td>12</td>
-          </tr>
-
+          {displayedDates.map(week => getWeekTr(week))}
         </tbody>
       </table>
     </div>
   );
+
+  function getWeekTr(week) {
+    return (
+    <tr>
+      {week.map(d => {
+        const isOtherMonth = d < 0;
+        const td = <td className={isOtherMonth && 'ui-datepicker-other-month'
+          || d === today && 'ui-datepicker-today'}>{isOtherMonth ? -d : d}</td>;
+        return td;
+      })}
+    </tr>);
+  }
 
   function getWeekDay() {
     const weekDays = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
