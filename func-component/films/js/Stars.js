70e2d77618b4c984e9447c8d0488b65453d3ba88
Nikita Skrebets <angelhearted@ya.ru>
2018-07-01 14:29:10 +0300

    Completed stars

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,7 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function Stars({count}) {
+  if (!(count >= 1 && count <= 5)) return null;
+  const stars = new Array(count).fill(<li><Star /></li>);
+  return <ul className="card-body-stars u-clearfix">{stars}</ul>;
 }
