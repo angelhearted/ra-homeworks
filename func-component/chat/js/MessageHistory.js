9d4a74c237ceb7db78f3c60bc4fcd205d6a51f6f
Nikita Skrebets <angelhearted@ya.ru>
2018-07-02 13:53:15 +0300

    Implemented MessageHistory component

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,17 @@
 'use strict';
+function MessageHistory({list}) {
+  const components = {
+    message: Message,
+    response: Response,
+    typing: Typing
+  };
+
+  return (
+    <ul>  
+      {list.map(msg => {
+        const Component = components[msg.type];
+        return <Component from={msg.from} message={{'time': msg.time, 'text': msg.text}} />
+      })}
+    </ul>
+  );
+}
