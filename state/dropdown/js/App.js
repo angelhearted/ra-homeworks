11f106c1da3bf027e5718b52dba8c769a7a54c09
Nikita Skrebets <angelhearted@ya.ru>
2018-07-11 02:29:43 +0300

    Completed transition to ES6 style of App

--- state/dropdown/js/App.js
+++ state/dropdown/js/App.js
@@ -1,33 +1,34 @@
-var App = React.createClass({
-  defaultProps: {
-    options: []
-  },
-  getInitialState: function () {
-    return {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       active: this.props.options[0],
       open: false
-    };
-  },
-  handleChange: function (option) {
+    }
+  }
+
+  handleChange(option) {
     this.setState({
       active: option
     });
-  },
-  toggleOpen: function () {
+  }
+
+  toggleOpen() {
     this.setState({
       open: !this.state.open
     });
-  },
-  render: function () {
+  }
+
+  render() {
     return (
       <div className="container">
         <div className={`dropdown-wrapper ${this.state.open ? "open" : ""}`} >
-          <button className={"btn"} onClick={this.toggleOpen} >
+          <button className={"btn"} onClick={this.toggleOpen.bind(this)} >
             <span>Account Settings</span>
             <i className="material-icons">public</i>
           </button>
           <ul className="dropdown">
-            {this.props.options.map((option, i) => (
+            {this.props.options.map(option => (
               <li
                 className={option === this.state.active ? "active" : ""}
                 onClick={() => this.handleChange(option)} >
@@ -39,4 +40,9 @@ var App = React.createClass({
       </div>
     );
   }
-});
+}
+
+App.defaultProps = {
+  options: [],
+  open: false
+}
