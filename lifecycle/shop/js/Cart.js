ef804ed103d6168f6ce2663294496f628dc4e2c8
Nikita Skrebets <angelhearted@ya.ru>
2018-08-27 13:39:52 +0300

    Completed cart should update functionality; fixed absent key warning; fixed missing semicolon typo.

--- lifecycle/shop/js/Cart.js
+++ lifecycle/shop/js/Cart.js
@@ -1,4 +1,8 @@
 class Cart extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    return this.props.isOpen !== nextProps.isOpen
+      || nextProps.isOpen;
+  }
 
   render() {
     return (
