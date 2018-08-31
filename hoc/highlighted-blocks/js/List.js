94fe6f9f25f9485fc59deac5022a297015f85c48
Nikita Skrebets <angelhearted@ya.ru>
2018-08-31 22:10:39 +0300

    Completed; initial version.

--- hoc/highlighted-blocks/js/List.js
+++ hoc/highlighted-blocks/js/List.js
@@ -1,17 +1,25 @@
 'use strict';
 
 const List = props => {
-    return props.list.map(item => {
-        switch (item.type) {
-            case 'video':
-                return (
-                    <Video {...item} />
-                );
-
-            case 'article':
-                return (
-                    <Article {...item} />
-                );
-        }
-    });
+  return props.list.map(item => withStyle(item, getComponent));
 };
+
+function getComponent(item) {
+  return item.type === 'video'
+      ? Video
+      : item.type === 'article'
+      ? Article
+      : null;
+}
+
+function withStyle(item, componentGetter) {
+  const Styler = item.views >= 1000
+      ? Popular
+      : item.views >= 100
+      ? New
+      : null;
+  const Component = componentGetter(item);
+  return Styler
+      ? <Styler><Component {...item}/></Styler>
+      : <Component {...item}/>;
+}
