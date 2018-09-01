974a01e39df5c0f9531477267154e7c75dafd41d
Nikita Skrebets <angelhearted@ya.ru>
2018-09-01 19:02:18 +0300

    Completed

--- hoc/relative-time/js/Video.js
+++ hoc/relative-time/js/Video.js
@@ -4,7 +4,17 @@ const Video = props => {
     return (
         <div className="video">
             <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            <DateTime date={props.date} />
+            <DateTimePretty date={props.date} />
         </div>
     )
 };
+
+const DateTimePretty = ({date}) => {
+    const dateDiff = Date.now() - Date.parse(date);
+    const datePretty = dateDiff < 60 * 60 * 1000
+        ? Math.ceil(dateDiff / 60 / 1000) + ' мтнут назад'
+        : dateDiff < 24 * 60 * 60 * 1000
+        ? Math.ceil(dateDiff / 60 / 60 / 1000) + ' часов назад'
+        : Math.ceil(dateDiff / 24 / 60 / 60 / 1000) + ' дней назад';
+    return <DateTime date={datePretty} />
+}
