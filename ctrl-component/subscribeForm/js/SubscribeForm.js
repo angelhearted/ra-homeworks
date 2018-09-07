ca8a852148adaf7e74465161ff59bca9c089087c
angelhearted <angelhearted@ya.ru>
2018-09-07 14:37:30 +0300

    Changes according to new comments. Removed ref, made more obvious state, simplified (probably with bug, but as instructor told) handleInput.

--- ctrl-component/subscribeForm/js/SubscribeForm.js
+++ ctrl-component/subscribeForm/js/SubscribeForm.js
@@ -2,39 +2,27 @@ class SubscribeForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isValid: false,
-      isError: false
+      validityClass: '',
+      errorClass: ''
     }
   }
 
-  handleInput(e) {
-    if (e.target.value === '') {
-      this.setState({
-        isValid: false,
-        isError: false
-      });
-    } else if (e.target.validity.valid) {
-      this.setState({
-        isValid: true,
-        isError: false
-      });
-    } else {
-      this.setState({
-        isValid: false,
-        isError: true
-      });
-    }
+  handleInput = e => {
+    this.setState(
+      e.target.validity.valid
+      ? { validityClass: 'is-valid', errorClass: ''}
+      : { validityClass: '', errorClass: 'is-error'}
+    );
   };
 
   render() {
     return (
       <div className="subscribe__form">
-        <form className={`form form--subscribe ${this.state.isValid ? 'is-valid' : ''} ${this.state.isError ? 'is-error' : ''}`}
-          ref={el => this.form = el}>
+        <form className={`form form--subscribe ${this.state.validityClass} ${this.state.errorClass}`}>
           <h4 className="form-title">Подписаться:</h4>
           <div className="form-group">
             <label htmlFor="input-email" className="sr-only">Email</label>
-            <input type="email" id="input-email" placeholder="Email" className="form-control" onChange={this.handleInput.bind(this)}/>
+            <input type="email" id="input-email" placeholder="Email" className="form-control" onChange={this.handleInput}/>
             <div className="form-error">Пожалуйста, проверьте корректность адреса электронной почты</div>
             <button type="submit" className="form-next">
               <i className="material-icons">keyboard_arrow_right</i>
