c484efc992b2b6d87454ea5ce41de4351db159f5
angelhearted <angelhearted@ya.ru>
2018-10-01 02:16:45 +0300

    Removed data from params of inner handler; changed indexOf to includes

--- events/feedback/js/FeedbackForm.js
+++ events/feedback/js/FeedbackForm.js
@@ -2,15 +2,18 @@
 function FeedbackForm({data, onSubmit}) {
 
   let form;
-  const formHandler = (data, e) => {
+  const handleSubmit = e => {
     e.preventDefault();
-    data.salutation = form.salutation.value;
-    data.name = form.name.value;
-    data.email = form.email.value;
-    data.subject = form.subject.value;
-    data.message = form.message.value;
-    data.snacks = Array.from(form.snacks).filter(el => el.checked).map(el => el.value);
-    onSubmit(JSON.stringify(data));
+  
+    const clientMsg = {};
+    clientMsg.salutation = form.salutation.value;
+    clientMsg.name = form.name.value;
+    clientMsg.email = form.email.value;
+    clientMsg.subject = form.subject.value;
+    clientMsg.message = form.message.value;
+    clientMsg.snacks = Array.from(form.snacks).filter(el => el.checked).map(el => el.value);
+  
+    onSubmit(JSON.stringify(clientMsg));
   };
 
   return (
@@ -47,12 +50,12 @@ function FeedbackForm({data, onSubmit}) {
       </div>
       <div className="contact-form__input-group">
         <p className="contact-form__label--checkbox-group">Хочу получить:</p>
-        <input className="contact-form__input contact-form__input--checkbox" id="snacks-pizza" name="snacks" type="checkbox" value="пицца" defaultChecked={Array.from(data.snacks).indexOf('пицца') > -1}/>
+        <input className="contact-form__input contact-form__input--checkbox" id="snacks-pizza" name="snacks" type="checkbox" value="пицца" defaultChecked={Array.from(data.snacks).includes('пицца')}/>
         <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-pizza">Пиццу</label>
-        <input className="contact-form__input contact-form__input--checkbox" id="snacks-cake" name="snacks" type="checkbox" value="пирог" defaultChecked={Array.from(data.snacks).indexOf('пирог') > -1}/>
+        <input className="contact-form__input contact-form__input--checkbox" id="snacks-cake" name="snacks" type="checkbox" value="пирог" defaultChecked={Array.from(data.snacks).includes('пирог')}/>
         <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-cake">Пирог</label>
       </div>
-      <button className="contact-form__button" type="submit" onClick={formHandler.bind(this, data)}>Отправить сообщение!</button>
+      <button className="contact-form__button" type="submit" onClick={e => handleSubmit(e)}>Отправить сообщение!</button>
       <output id="result" />
     </form>
   );
