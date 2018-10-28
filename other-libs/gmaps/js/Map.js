faddd113cf4ed539f513d0b342032c9b686876a2
Nikita Skrebets <angelhearted@ya.ru>
2018-10-29 00:54:06 +0300

    Completed adding map

--- other-libs/gmaps/js/Map.js
+++ other-libs/gmaps/js/Map.js
@@ -1,9 +1,34 @@
 class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { map: null };
+  }
+
+  componentDidMount() {
+    this.setState({
+      map: new google.maps.Map(document.getElementById('map'))
+    });
+  }
+
+  componentDidUpdate() {
+    this.state.map.setZoom(5);
+    this.props.points.forEach(p => new google.maps.Marker({
+      map: this.state.map,
+      position: p,
+      visible: true
+    }));
+    this.state.map.setCenter({ lng: 0, lat: 0 });
+  }
+
+  componentWillUnmount() {
+    this.map.destroy();
+  }
+
   render() {
     return (
-      <div>
+      <div id='mapParent'>
         Карта
-        {this.props.points.map(office => <p>{JSON.stringify(office)}</p>)}
+        <div id='map'></div>
       </div>
     );
   }
